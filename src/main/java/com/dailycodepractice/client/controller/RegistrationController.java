ea3fe144428c47489894e1469a8f81a5b2455a89
Anurag <anurag.baranwal@Kuliza.com>
2023-03-16 22:38:50 +0530

    add user verication

--- src/main/java/com/dailycodepractice/client/controller/RegistrationController.java
+++ src/main/java/com/dailycodepractice/client/controller/RegistrationController.java
@@ -6,9 +6,7 @@ import com.dailycodepractice.client.model.UserModel;
 import com.dailycodepractice.client.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -27,6 +25,14 @@ public class RegistrationController {
                 user,apliactionUrl(request)
         ));
         return "Success";
+    }
+    @GetMapping("/verifyRegistration")
+    public String verifyRegistration(@RequestParam("token") String token){
+        String result = userService.validateVerificationToken(token);
+        if (result.equalsIgnoreCase("valid")){
+            return "User Verified Successfully";
+        }
+        return "Bad User Credential";
     }
 
     private String apliactionUrl(HttpServletRequest request) {
--- src/main/java/com/dailycodepractice/client/service/UserServiceImpl.java
+++ src/main/java/com/dailycodepractice/client/service/UserServiceImpl.java
@@ -9,8 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Calendar;
+
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
@@ -18,6 +20,7 @@ public class UserServiceImpl implements UserService{
     private VerificationTokenRepository verificationTokenRepository;
     @Autowired
     private PasswordEncoder passwordEncoder;
+
     @Override
     public User registerUser(UserModel userModel) {
         User user = new User();
@@ -34,8 +37,30 @@ public class UserServiceImpl implements UserService{
     @Override
     public void saveVerificationTokenForUser(String token, User user) {
         VerificationToken verificationToken =
-                new VerificationToken(user,token);
+                new VerificationToken(user, token);
 
         verificationTokenRepository.save(verificationToken);
+    }
+
+    @Override
+    public String validateVerificationToken(String token) {
+        VerificationToken verificationToken =
+                verificationTokenRepository.findByToken(token);
+        if (verificationToken == null) {
+            return "invalid";
+        }
+
+        User user = verificationToken.getUser();
+        Calendar cal = Calendar.getInstance();
+
+        if ((verificationToken.getExpirationTime().getTime()
+                - cal.getTime().getTime()) <= 0) {
+            verificationTokenRepository.delete(verificationToken);
+            return "expired";
+        }
+
+        user.setEnabled(true);
+        userRepository.save(user);
+        return "valid";
     }
 }
