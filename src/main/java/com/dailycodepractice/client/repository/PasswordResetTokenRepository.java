0e6e184105c9c74d01f161b944318963b0fdc5eb
rag678 <87017032+rag678@users.noreply.github.com>
2023-03-19 18:26:27 +0530

    Revert "password function added"

--- src/main/java/com/dailycodepractice/client/repository/PasswordResetTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dailycodepractice.client.repository;
-
-import com.dailycodepractice.client.entity.PasswordResetToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken,Long> {
-    PasswordResetToken findByToken(String token);
-}
--- src/main/java/com/dailycodepractice/client/service/UserService.java
+++ src/main/java/com/dailycodepractice/client/service/UserService.java
@@ -1,10 +1,7 @@
 package com.dailycodepractice.client.service;
 
 import com.dailycodepractice.client.entity.User;
-import com.dailycodepractice.client.entity.VerificationToken;
 import com.dailycodepractice.client.model.UserModel;
-
-import java.util.Optional;
 
 public interface UserService {
     User registerUser(UserModel userModel);
@@ -12,18 +9,4 @@ public interface UserService {
     void saveVerificationTokenForUser(String token, User user);
 
     String validateVerificationToken(String token);
-
-    VerificationToken generateNewVerificationToken(String oldToken);
-
-    User findUserByEmail(String email);
-
-    void createPasswordResetTokenForUser(User user, String token);
-
-    String validatePasswordResetToken(String token);
-
-    Optional<User> getUserByPasswordResetToken(String token);
-
-    void changePassword(User user, String newPassword);
-
-    boolean checkIfValidOldPassword(User user, String oldPassword);
 }
